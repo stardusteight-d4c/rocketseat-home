8ae007411611c61ed4c704037b3943e629dd730f
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-12-29 11:46:14 -0300

    chore: create base layout of Faq section

---
+++ src/modules/home/components/Faq/Faq.tsx
@@ -0,0 +1,30 @@
+import { FaqItem } from "./components/FaqItem"
+import { useFaq } from "./hooks/useFaq"
+
+interface FaqProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const Faq: React.FC<FaqProps> = ({ className, ...props }: FaqProps) => {
+  const { faqData } = useFaq()
+
+  return (
+    <section className={"py-20 bg-dark-str " + className} {...props}>
+      <div className="flex items-start gap-x-[154px] max-w-[1230px] w-full mx-auto">
+        <div className="flex flex-col items-start gap-y-4">
+          <div className="border border-gray-600/[0.50] bg-gradient-to-t from-dark-low to-dark-mid shadow-sm shadow-black/50 rounded-lg p-2 w-fit">
+            <span className="font-bold uppercase bg-gradient-to-tr text-transparent from-brand-primary to-brand-secondary bg-clip-text">
+              FAQ_
+            </span>
+          </div>
+          <h2 className="text-4xl whitespace-nowrap font-bold leading-[40px] mt-6 w-full text-white">
+            Perguntas e respostas <br /> mais frequentes
+          </h2>
+        </div>
+        <div className="flex flex-col w-full">
+          {faqData.map((item, index) => (
+            <FaqItem key={index} data={item} position={index + 1} />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
