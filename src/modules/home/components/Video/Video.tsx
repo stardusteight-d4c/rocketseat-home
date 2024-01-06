ef2572df40d4b6f8e7c80159dd390a2115daa697
Gabriel Sena <stardusteight.d4cc@gmail.com>
2024-01-06 12:00:09 -0300

    chore: importing showcase mp4 asset

--- src/modules/home/components/Video/Video.tsx
+++ src/modules/home/components/Video/Video.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import rocketShowcase from "@/public/rocket-showcase.png"
 import { Button } from "@/shared/components/atoms/ui/Button"
+import { useState } from "react"
 
 interface VideoProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -7,6 +10,8 @@ export const Video: React.FC<VideoProps> = ({
   className,
   ...props
 }: VideoProps) => {
+  const [play, setPlay] = useState<boolean>(false)
+
   return (
     <section
       className={
@@ -16,18 +21,34 @@ export const Video: React.FC<VideoProps> = ({
       {...props}
     >
       <div className="relative mx-3 lg:mx-0 h-[175px] md:h-[200px] lg:h-[458px] max-w-[100vw]">
-        <div className="absolute lg:-mt-[214px] -bottom-2 lg:bottom-auto z-[50] left-1/2 border-[8px] lg:border-[10px] max-w-full lg:max-w-[1230px] w-full overflow-y-hidden h-[200px] xxs:h-[250px] xs:h-[300px] md:h-[400px] lg:h-[682px] -translate-x-1/2 border-dark-str rounded-b-none rounded-lg  mx-auto">
+        <div className="absolute lg:-mt-[214px] bottom-0 z-[50] left-1/2 border-[8px] !border-b-0 lg:border-[10px] max-w-full lg:max-w-[1230px] w-full overflow-y-hidden h-[200px] xxs:h-[250px] xs:h-[300px] md:h-[400px] lg:h-[690px] -translate-x-1/2 bg-dark-str border-dark-str rounded-b-none rounded-lg mx-auto">
           <div className="relative">
-            <img
-              src={rocketShowcase.src}
-              alt="rocket_showcase"
-              className="brightness-75 w-full lg:rounded-t-lg h-[200px] xxs:h-[250px] xs:h-[300px] md:h-[400px] lg:h-[682px]"
-            />
-            <Button
-              type="play"
-              title="Aperte o play e conheça a Rocketseat"
-              className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-white"
-            />
+            {!play && (
+              <>
+                <img
+                  src={rocketShowcase.src}
+                  alt="rocket_showcase"
+                  className="brightness-75 w-full lg:rounded-t-lg h-[200px] xxs:h-[250px] xs:h-[300px] md:h-[400px] lg:h-[682px]"
+                />
+                <div
+                  onClick={() => setPlay(true)}
+                  className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-white"
+                >
+                  <Button
+                    type="play"
+                    title="Aperte o play e conheça a Rocketseat"
+                  />
+                </div>
+              </>
+            )}
+            {play && (
+              <video
+                autoPlay
+                controls
+                src="/rocketseat-showcase.mp4"
+                className="brightness-75 min-w-full min-h-full lg:rounded-t-lg h-[200px] xxs:h-[250px] xs:h-[300px] md:h-[400px] lg:h-[682px]"
+              />
+            )}
           </div>
         </div>
       </div>
